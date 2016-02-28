8f0ddca2ba2a987ddbeaacd076de308cf08e5d87
Ahmed Saad <ahsaeldin@gmail.com>
2016-02-28 15:37:40 +0200

    no message

---
+++ Spiders/Scrapy/Dillards/run.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import os
+import sys
+import time
+from scrapy import cmdline
+
+
+name = 'dillards'
+if __name__ == '__main__':
+    command = "scrapy crawl {0} -t csv -o outs.csv".format(name).split()
+    cmdline.execute(command)
+
